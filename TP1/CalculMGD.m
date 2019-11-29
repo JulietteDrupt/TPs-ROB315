c11768169667ce76f356aefa6250ca38e5f203df
Juliette Drupt <juliette.drupt@etu.emse.fr>
2019-11-29 16:11:55 +0100

    .

---
+++ TP1/CalculMGD.m
@@ -0,0 +1,13 @@
+function [g_06,g_elem] = CalculMGD(alpha,d,theta,r)
+%CALCULMGD : calcule le MGD d'un robot quelconque en chaîne ouverte simple
+
+    g_06 = eye(4);
+    N = length(alpha);
+    g_elem = cell(1,N);
+
+    for i=1:N
+        g_elem(i) = CalcultTransformationElem(alpha(i),d(i),theta(i),r(i));
+        g_06 = g_06*g_elem(i);
+    end 
+end
+
